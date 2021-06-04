77dd6fbed48dcb7353d7946b4430e6e8d9c5bf42
ahsan <m.ahsan.nazer@gmail.com>
2021-06-04 14:36:29 +1200

    saving changes to accounts.jsx
    Account.jsx now received three different kinds of orders from database;
    cancelled, pending, resolved (accepted/rejected).

--- pages/account.jsx
+++ pages/account.jsx
@@ -13,6 +13,36 @@ function getTableRow(order, key){
     <td className="bg-info">{order.id}</td>
     </tr>)
 }
+
+async function getOrderTable(orders, tableName){
+        let i=0;
+        const ordersTable = orders.map(order => {
+            i+=1;
+            return getTableRow(order, i)
+        });
+        return(<table className="table table-hover table-sm" key={tableName}>
+            <thead><tr key="headRow">
+            <th className="bg-primary">have</th>
+            <th className="bg-primary">Amount</th>
+            <th className="bg-secondary">want</th>
+            <th className="bg-secondary">Amount</th>
+            <th className="bg-info">Order id</th>
+            </tr></thead>
+            <tbody>{ordersTable}</tbody> 
+            </table>
+        )
+}
+
+function setOrdersHeading(displayedOrders){
+    switch(displayedOrders){
+                case 'myOrders':
+                    return <h4>Pending Orders</h4>
+                case 'myCancelledOrders':
+                    return <h4>Cancelled Orders</h4>
+                case 'myPastOrders':
+                    return <h4>Past Orders</h4>
+            }
+}
 export default function AccountPage(props){
     // console.log("props: ", props);
     // console.log("props.router: ", props.router);
@@ -30,27 +60,28 @@ export default function AccountPage(props){
 
     const me = new User(user, tokens);
     const [myOrders, setMyOrders] = useState('');
+    const [myCancelledOrders, setMyCancelledOrders] = useState('');
+    const [myPastOrders, setMyPastOrders] = useState('');
+    const [displayedOrders, setDisplayedOrders] = useState('myOrders');
+    const allOrders = {myOrders, myCancelledOrders, myPastOrders};
+
     useEffect(async ()=> {
-        const orders = await getMyOrders(me);
-        let i=0;
-        const ordersTable = orders.map(order => {
-            i+=1;
-            // return <div key={i}>{order.haveAmount}</div>
-            return getTableRow(order, i)
-        });
-        setMyOrders(<table className="table table-hover table-sm">
-            <thead><tr key="headRow">
-            <th className="bg-primary">have</th>
-            <th className="bg-primary">Amount</th>
-            <th className="bg-secondary">want</th>
-            <th className="bg-secondary">Amount</th>
-            <th className="bg-info">Order id</th>
-            </tr></thead>
-            <tbody>{ordersTable}</tbody> 
-            </table>);
+        const {orders} = await getMyOrders(me);
+        setMyOrders(await getOrderTable(orders.pendingOrders, "pending"));
+        setMyCancelledOrders(await getOrderTable(orders.cancelledOrders, "cancelled"));
+        setMyPastOrders(await getOrderTable(orders.pastOrders, "past"));
     },
     []
     );
+
+    async function paginate(){
+        return(<nav key="orderStatusPages"><ul className="pagination">
+               <li className="page-item"><a className="page-link" href="#">Pending Orders</a></li>
+               <li className="page-item"><a className="page-link" href="#">Cancelled Orders</a></li>
+               <li className="page-item"><a className="page-link" href="#">Past Orders</a></li>
+            </ul></nav>)
+    }
+
     const curList = "NZD,AUD,EUR,USD,CAD,CNY,KRW".split(",");
     const [buyCur, setBuyCur] = useState(curList);
     const [buyValue, setBuyValue] = useState('AUD')
@@ -63,14 +94,14 @@ export default function AccountPage(props){
         <>
         <NavBarUser/>
         <div className="container mt-5 mb-5">
-        <h1>Profile</h1>
+        <h2>Profile</h2>
         <div className="container bg-light mt-4 mb-4 p-2">
-        <p>My name is {user.name}</p>
+        <p>My username is {user.name}</p>
         <p>My email is {user.email}</p>
         <p>My rating is ...</p>
         </div>
         <h2>Create a new order</h2>
-        <div>
+        <div className="container mb-4" id="newOrder">
             {user.isEmailVerified == false && 
             <div className="alert alert-danger m-2">An email has been dispatched to your email account. 
                 You can not transact until you have verified your email account. 
@@ -125,10 +156,27 @@ export default function AccountPage(props){
         </div>
         </div>
         </div>
-        <h3>My transactions</h3>
-        { 
-        myOrders 
-        }
+        <h2>My transactions</h2>
+        <div className="container mb-4" id="transactions">
+        <nav key="orderStatusPages"><ul className="pagination">
+               <li className="page-item"><a className="page-link " href="#" onClick={(e)=>{e.preventDefault();setDisplayedOrders("myOrders"); 
+               }}>Pending Orders</a></li>
+               <li className="page-item"><a className="page-link" href="#" onClick={(e)=>{e.preventDefault(); setDisplayedOrders("myCancelledOrders");
+               }}>Cancelled Orders</a></li>
+               <li className="page-item"><a className="page-link" href="#" onClick={(e)=>{e.preventDefault(); setDisplayedOrders("myPastOrders");
+               }}>Past Orders</a></li>
+        </ul></nav>
+        {/* <div id="pendingOrders"> <h4>Pending Orders</h4> { myOrders } </div> */}
+        {/* <div id="cancelledOrders"> <h4>Cancelled Orders</h4> { myCancelledOrders } </div> */}
+        {/* <div id="pastOrders"><h4>Past Orders</h4>{ myPastOrders } </div> */}
+        <div id="displayedOrders">
+            {setOrdersHeading(displayedOrders)}
+            {allOrders[displayedOrders]}
+        </div>
+        </div>
+        <div>
+        </div>
+        
         </div>
         </>
     )
