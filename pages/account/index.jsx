6f97bcaca3d2dd80fe974d04e3d2b8cb84380f4f
ahsan <m.ahsan.nazer@gmail.com>
2021-06-13 23:11:45 +1200

    major change in account.jsx
    Cleaned up account.jsx and moved create order form to a react class
    component.

--- pages/account/index.jsx
+++ pages/account/index.jsx
@@ -1,10 +1,118 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, Component} from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
 import 'font-awesome/css/font-awesome.min.css';
 import NavBarUser from "/components/navbaruser";
 import User, {readUserInfoFromStorage } from "/beapi/users";
+
+const orderCard = (amount, cardKey, currency, currencies, disabled, 
+    inputName, label, onInputChange, onSelectChange, selectName
+    )=>{
+    return (
+        <div className="card-body  bg-light border rounded border-secondary mb-4 " key={cardKey}>
+            <label htmlFor={inputName} className="form-label"><h5>{label}</h5></label>
+            <div className="input-group">
+                <input
+                    className="form-control m-2"
+                    disabled={disabled}
+                    id={inputName}
+                    name={inputName}
+                    onChange={onInputChange}
+                    type="text"
+                    value={amount} 
+                />
+                <select
+                    className="form-select m-2"
+                    disabled={disabled}
+                    name={selectName}
+                    value={currency}
+                    onChange={onSelectChange}
+                >
+                    {currencies.map(currency=>(
+                        <option
+                            key ={currency}
+                            value={currency}
+                        >
+                            {currency}
+                        </option>
+                        ))
+                    }
+                </select>
+            </div>
+        </div>
+    );
+}
+
+class CreateOrderForm extends Component{
+    constructor(props){
+        super();
+        this.state = {
+            haveAmount: 100,
+            haveCurrency: 'NZD',
+            wantAmount: 95,
+            wantCurrency: 'AUD',
+            currencies:  "NZD,AUD,EUR,USD,CAD,CNY,KRW".split(","),
+            orderCreationMessage: "",
+        }
+    }
+    async createOrder(haveCur, haveAmount, wantCur, wantAmount){
+        try{
+            const orderBody = {have: haveCur.toLowerCase(), haveAmount, want: wantCur.toLowerCase(), wantAmount};
+            const res = await this.props.user.createUserOrder(orderBody);
+            if (res.status == 201){
+                //returns order body
+                // const resData = await res.json();
+                this.setState({orderCreationMessage: <div className="alert alert-success mt-2 ">Your order was created</div>});
+            }
+        }
+        catch(err){
+            console.log("(Bad stuff) catching all else!");
+        }
+    }
+    handleInputChange = (e)=>{
+        e.preventDefault();
+        const inputName = e.currentTarget.name;
+        const value = e.currentTarget.value;
+        this.setState({[inputName]: value});
+    }
+    handleSelectChange = (e)=>{
+        e.preventDefault();
+        const selectName = e.currentTarget.name;
+        const option = e.currentTarget.value;
+        this.setState({[selectName]: option});
+    }
+    handleClick = (e)=>{
+        e.preventDefault();
+        const {haveCurrency, haveAmount, wantCurrency, wantAmount} = {...this.state};
+        this.createOrder(haveCurrency, haveAmount, wantCurrency, wantAmount);
+    }
+    componentDidUpdate(){
+        if (this.state.orderCreationMessage !== ''){
+            setTimeout( ()=>this.setState({orderCreationMessage: ""}), 4000);
+        }
+    }
+    render(){
+        const disabled = !this.props.user.user.isEmailVerified;
+        return(
+        <div key="newOrderForm" className="bg-white border border-dark rounded m-2 p-2 col col-md-6 ">
+            {
+            orderCard(
+            this.state.haveAmount, 'haveAmount', this.state.haveCurrency, this.state.currencies, disabled, 
+            'haveAmount', 'I have', this.handleInputChange, this.handleSelectChange, 'haveCurrency')
+            }
+            {
+            orderCard(
+            this.state.wantAmount, 'wantAmount', this.state.wantCurrency, this.state.currencies, disabled, 
+            'wantAmount', 'I want', this.handleInputChange, this.handleSelectChange, 'wantCurrency')
+            }
+            <div><button className="btn btn-info border border-dark"  disabled={disabled}
+            onClick={this.handleClick}>Create Order
+            </button></div>
+            {this.state.orderCreationMessage}
+        </div>)
+            }
+}
 
 function getTableRow(order, key, wrapInButton ){
     let id = order.id;
@@ -75,109 +183,15 @@ function AccountPage(){
     const [displayedOrders, setDisplayedOrders] = useState('myOrders');
     const allOrders = {myOrders, myCancelledOrders, myPastOrders};
 
-    const curList = "NZD,AUD,EUR,USD,CAD,CNY,KRW".split(",");
-    const [haveCurList, setHaveCurList] = useState(curList);
-    const [haveCur, setHaveCur] = useState('NZD')
-    const [haveAmount, setHaveAmount] = useState(100);
-    const [wantCurList, setWantCurList] = useState(curList);
-    const [wantCur, setWantCur] = useState('AUD')
-    const [wantAmount, setWantAmount] = useState(100);
-    const [orderCreationMessage, setOrderCreationMessage ] = useState('');
-
     useEffect(async ()=> {
         const {orders} = await getMyOrders(me);
         setMyOrders(await getOrderTable(orders.pendingOrders, "pending", true ));
         setMyCancelledOrders(await getOrderTable(orders.cancelledOrders, "cancelled"));
         setMyPastOrders(await getOrderTable(orders.pastOrders, "past"));
 
-        if (orderCreationMessage !== ''){
-            setTimeout( ()=>setOrderCreationMessage(""), 4000);
-        }
     },
-    [orderCreationMessage]
+    []
     );
-
-    async function paginate(){
-        return(<nav key="orderStatusPages"><ul className="pagination">
-               <li className="page-item"><a className="page-link" href="#">Pending Orders</a></li>
-               <li className="page-item"><a className="page-link" href="#">Cancelled Orders</a></li>
-               <li className="page-item"><a className="page-link" href="#">Past Orders</a></li>
-            </ul></nav>)
-    }
-
-    function getNewOrderForm(){
-        const haveCard = <div className="card-body  bg-light border rounded border-secondary mb-4 " key="haveCard">
-            <div className="card-title"><h5>I have</h5></div>
-            <div className="input-group">
-                <input value={haveAmount} 
-                onChange={e=> setHaveAmount(e.currentTarget.value)} 
-                className="form-control m-2"
-                />
-            <select 
-            disabled={!user.isEmailVerified}
-            value={haveCur}
-            onChange={ e=> setHaveCur(e.currentTarget.value)}
-            className="form-select m-2"
-            >
-                {haveCurList.map( curr=> (
-                <option
-                key = {curr}
-                value={curr}
-                >
-                    {curr}
-                </option>
-                ))}
-            </select>
-            </div>
-            </div>
-        const wantCard = <div className="card-body bg-light border rounded border-secondary mb-4" key="wantCard">
-            <div className="card-title"><h5>I want </h5></div>
-            <div className="input-group">
-                <input value={wantAmount} 
-                onChange={e=> setWantAmount(e.currentTarget.value)} 
-                className="form-control m-2"
-                />
-            <select 
-            disabled={!user.isEmailVerified}
-            value={wantCur}
-            onChange={ e=> setWantCur(e.currentTarget.value)}
-            className="form-select m-2"
-            >
-                {wantCurList.map( curr=> (
-                <option
-                key = {curr}
-                value={curr}
-                >
-                    {curr}
-                </option>
-                ))}
-            </select>
-            </div>
-            </div>
-        return(<div key="newOrderForm" className="bg-white border border-dark rounded m-2 p-2 col col-md-6 ">
-            {haveCard}
-            {wantCard}
-            <div><button className="btn btn-info border border-dark"  disabled={!user.isEmailVerified }
-            onClick={(e)=>{
-                e.preventDefault();
-                createOrder(haveCur, haveAmount, wantCur, wantAmount);
-                }}>Create Order
-            </button></div>
-            {orderCreationMessage}
-        </div>)
-    }
-    
-    async function createOrder(haveCur, haveAmount, wantCur, wantAmount){
-        const orderBody = {have: haveCur.toLowerCase(), haveAmount, want: wantCur.toLowerCase(), wantAmount};
-        console.log("order details: ", haveAmount)
-        const res = await me.createUserOrder(orderBody);
-        if (res.status == 201){
-            const resData = await res.json();
-            setOrderCreationMessage(<div className="alert alert-danger mt-2 ">Your order was created</div>
-            );
-        console.log("Order creation was: ", resData);
-        }
-    }
 
     return(
         <>
@@ -205,8 +219,8 @@ function AccountPage(){
                 You can not transact until you have verified your email account. 
             </div>
             }
-            {getNewOrderForm()}
         </div>
+        <CreateOrderForm user={me}/>
         <h2>My transactions</h2>
         <div className="container mb-4" id="transactions">
         <nav key="orderStatusPages"><ul className="pagination">
